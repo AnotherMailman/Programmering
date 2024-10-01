27cf88b2425b8f26500d60f2b386e28fe6980309
AnotherMailman <144328915+AnotherMailman@users.noreply.github.com>
2024-10-01 11:44:47 +0200

    Add files via upload

--- number.py
+++ number.py
@@ -1,11 +1,25 @@
-numbers=[1,7,33,6,88,-7,25,13,7]
-even=[]
-odd=[]
-for i in numbers:
-    if i % 2 == 0:
-        even.append(i)
-    else:
-        odd.append(i)
+def addition(x,y):
+    return x+y
+def subtraktion(x,y):
+    return x-y
+def multiplikation(x,y):
+    return x*y
+def division(x,y):
+    return x/y
 
-
-print(even)
+print("Välkommen tll miniräknaren")
+print("Mata in 1 tal")
+tal1=int(input())
+print("Mata in ett annat tal")
+tal2=int(input())
+print("1 för addition, 2 för subtraktion, 3 för multiplikation, 4 för division")
+Arithmetic=int(input())
+if Arithmetic==1:
+    print(addition(tal1, tal2))
+elif Arithmetic==2:
+    print(subtraktion(tal1, tal2))
+elif Arithmetic==3:
+    print(multiplikation(tal1, tal2))
+elif Arithmetic==4:
+    print(division(tal1, tal2))
+else: print("ogiltig val")
